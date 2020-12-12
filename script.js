220d8c28868444c3f2fdb141b1b17fa1346629af
sugijotaro <sugi.jotaro@gmail.com>
2020-12-12 19:46:47 +0900

    PCから閲覧した時にQRコードを表示するようにした

---
+++ script.js
@@ -0,0 +1,13 @@
+var ta = document.getElementById('Terminal')
+var ut = navigator.userAgent;
+
+document.getElementById("qr").style.display ="none";
+const qr = document.getElementById("qr");
+
+if(ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0){
+    qr.style.display ="none";
+}else if(ut.indexOf('iPad') > 0 || ut.indexOf('Android') > 0){
+    qr.style.display ="none";
+}else{
+    qr.style.display ="block";
+}
