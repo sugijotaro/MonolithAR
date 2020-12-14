726ec4cb37fafefe3522bc21349917955c13c3f9
sugijotaro <sugi.jotaro@gmail.com>
2020-12-14 16:57:34 +0900

    インデント整形

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.onload = function(){
-var ta = document.getElementById('Terminal')
-var ut = navigator.userAgent;
+window.onload = function () {
+    var ta = document.getElementById('Terminal')
+    var ut = navigator.userAgent;
 
-document.getElementById("qr").style.display ="none";
-const qr = document.getElementById("qr");
+    document.getElementById("qr").style.display = "none";
+    const qr = document.getElementById("qr");
 
-if(ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0){
-    qr.style.display ="none";
-}else if(ut.indexOf('iPad') > 0 || ut.indexOf('Android') > 0){
-    qr.style.display ="none";
-}else{
-    qr.style.display ="block";
-}
+    if (ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0) {
+        qr.style.display = "none";
+    } else if (ut.indexOf('iPad') > 0 || ut.indexOf('Android') > 0) {
+        qr.style.display = "none";
+    } else {
+        qr.style.display = "block";
+    }
 }
